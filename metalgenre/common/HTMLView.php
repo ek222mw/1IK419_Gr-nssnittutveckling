3c99f3a958c4c6b682a4bde5ed082993edab7082
Emil Karlsson <ek222mw@student.lnu.se>
2015-05-20 12:46:18 +0200

    fix mobil css

--- metalgenre/common/HTMLView.php
+++ metalgenre/common/HTMLView.php
@@ -12,7 +12,9 @@
 				Metalgenre.se
 				</title>
 				<meta charset='utf-8'>
-				<link rel='stylesheet' type='text/css' href='./css/bootstrap.css' />
+				<link rel='stylesheet' type='text/css' href='./css/bootstrap.css' media='screen and (min-width:481px)' />
+        		<link rel='stylesheet' type='text/css' href='./css/bootstrapmobil.css' media='screen and (max-width:481px)' />
+       			<link rel='stylesheet' type='text/css' href='./css/bootstrap.css' media='print' />
 				</head>
 				<body>
 				<header class='header'>
